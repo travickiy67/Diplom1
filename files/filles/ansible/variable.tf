25d603c1ff544b7adffb1322515d20435a71431b
travitskii <travickiy.67@gmail.com>
2025-06-12 14:16:20 +0300

    main, komment

--- files/filles/ansible/variable.tf
+++
@@ -1,5 +0,0 @@
-variable "ssh-authorized-keys" {
-  type        = string
-  description = "The AWS region to deploy resources in"
-  default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOYspe8XbSYQTqZ5Hz0euSC3zwD0nRVGqEou/T4tGNXn travitskii@ubuntu22"
-}
